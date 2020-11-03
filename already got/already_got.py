95abe6130593658d39bc2dc3a2d22e624ead784b
papalooo <68888789+papalooo@users.noreply.github.com>
2020-11-04 08:30:13 +0900

    wargame
    already got,
    f12 and read header

---
+++ already got/already_got.py
@@ -0,0 +1,13 @@
+
+
+# 문제
+# can you see HTTP Response header?
+# you've already got key! :p
+# = 넌 이미 키를 갖고 있다.
+
+# 풀이
+# 문제의 웹 페이지에서 F12(개발자 도구)
+# 를 활성화 시켜서 Network - Headers
+# 에서 FLAG 값을 확인할 수 있다.
+
+
