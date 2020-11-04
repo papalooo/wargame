6ff40a1de8e30d732df5b1b5e15664272d5a80eb
papalooo <68888789+papalooo@users.noreply.github.com>
2020-11-04 18:35:59 +0900

    wargame-flee button
    Press F12 and click Elements
    Remove style value of <div> item

---
+++ flee button/flee_button.py
@@ -0,0 +1,16 @@
+
+# 문제
+# click the button!
+# i can't catch it!
+# click button, 
+# if you want to get the authentication key
+# = 마우스를 피해 움직이는 버튼을 클릭하라
+
+# 풀이
+# F12(개발자 도구) 를 열어서 Elements 에서 
+# 마우스를 움직일 때 마다 변하는
+# <div> 항목의 left , top 값을 확인 한다.
+# <div> 항목의 style 의 값을 지우면 
+# 버튼이 움직이지 않는다.
+
+# 클릭하여 FLAG 값을 확인할 수 있다.
