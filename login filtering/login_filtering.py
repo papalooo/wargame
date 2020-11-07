733a3bdd640b1d0d97df665e5eab45cfd08a9a2c
papalooo <68888789+papalooo@users.noreply.github.com>
2020-11-08 00:38:00 +0900

    wargame-login filtering
    Since php is case sensitive, you can check the FLAG value by changing any letter to upper case in an account with restricted access.

---
+++ login filtering/login_filtering.py
@@ -0,0 +1,12 @@
+
+# 문제
+# I have accounts. but, it's blocked.
+# can you login bypass filtering?
+
+# 풀이
+# php 에선 대소문자를 구분하기 때문에
+# block 된 guest / guest 계정 이나
+# blueh4g / blueh4g1234ps 계정에서
+# 아무거나 대문자로 바꿔서 로그인하면 
+# FLAG 값을 확인할 수 있습니다.
+
