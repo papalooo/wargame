6febfa78d2be86c1cb52e024777fc0074f5f0e68
papalooo <68888789+papalooo@users.noreply.github.com>
2020-11-10 18:58:46 +0900

    wargame-QR Code
    Press F12 to check the FLAG value by scanning the qr code after receiving the qr code image in the Sources-top-wargame.kr-qr_code_puzzle-img folder

---
+++ QR Code Puzzle/QR_Code_Puzzle.py
@@ -0,0 +1,11 @@
+
+# 문제
+# javascript puzzle challenge
+# just enjoy
+# = 마구 섞인 qr 코드를 완성하면 된다.
+
+# 풀이
+# F12(개발자 도구)를 눌러서 
+# Sources - top - wargame.kr - qr_code_puzzle - img
+# 폴더의 qr 코드 이미지를 받은 후 
+# qr 코드를 스캔하면 FLAG 값을 확인할 수 있다.
